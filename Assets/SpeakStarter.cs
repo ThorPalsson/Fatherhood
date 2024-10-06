a2780c4bf1eecfeece0426ec2d00a30d640c46f1
Sævar Þór Pálsson <thorpalsson92@gmail.com>
2024-10-06 19:40:03 +0200

    a<erfcg5hyb

--- Assets/SpeakStarter.cs
+++ Assets/SpeakStarter.cs
@@ -5,9 +5,16 @@ public class SpeakStarter : MonoBehaviour
 {
    public string message;
    public int id = 0; 
+
+   public bool doesthing; 
    private void OnTriggerEnter(Collider other) {
         if (other.CompareTag("Player"))
         {
+            if(doesthing)
+            {
+                PlayerController.Instance.Sword.SetActive(true); 
+            }
+
             GameManager.Instance.StartConversation(message, id ); 
             Destroy(this.gameObject); 
         }
