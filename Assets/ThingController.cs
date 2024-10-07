001e4a2e354d20a537a8a14629b575c4c26177eb
Sævar Þór Pálsson <thorpalsson92@gmail.com>
2024-10-07 19:39:46 +0200

    fix

--- Assets/ThingController.cs
+++ Assets/ThingController.cs
@@ -21,20 +21,28 @@ public class ThingController : MonoBehaviour
         GameManager.Instance.ChangeSong(clip); 
         GameManager.Instance.StartConversation("So here we are", 888); 
         yield return new WaitForSeconds(6f);
+        GameManager.Instance.ChangePitch();
         GameManager.Instance.StartConversation("Just you and me once again!", 878); 
         yield return new WaitForSeconds(6f);
+        GameManager.Instance.ChangePitch();
         GameManager.Instance.StartConversation("You think congratulations are in order?", 885);
         yield return new WaitForSeconds(6f); 
+        GameManager.Instance.ChangePitch();
         GameManager.Instance.StartConversation("No, I will not congratulate you", 8858567);
         yield return new WaitForSeconds(6f);
+        GameManager.Instance.ChangePitch();
         GameManager.Instance.StartConversation("Look at yourself, you are a wretch", 884); 
         yield return new WaitForSeconds(6f); 
+        GameManager.Instance.ChangePitch();
         GameManager.Instance.StartConversation("Pitiful excuse of a father", 28656564); 
         yield return new WaitForSeconds(6f); 
+        GameManager.Instance.ChangePitch();
         GameManager.Instance.StartConversation("You have done NOTHING HERE", 838);
         yield return new WaitForSeconds(6f); 
+        GameManager.Instance.ChangePitch();
         GameManager.Instance.StartConversation("NOTHING I TELL YEE", 8884563);  
         yield return new WaitForSeconds(6f); 
+        GameManager.Instance.ChangePitch();
         SceneManager.LoadScene("EndingCutscene"); 
     }
 
